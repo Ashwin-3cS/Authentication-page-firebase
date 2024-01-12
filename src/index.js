af4b9f385f00b250c07fff3deb9c21e8694e2e53
Ashwin-3cS <flashashwin2004@gmail.com>
2024-01-13 02:58:10 +0530

    authentication succes , with firebase as backend

--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import  "./index.css";
+// import { createRoot } from 'react-dom/client';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+
+import App from "./App.js";
+
+import{
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
+import Protected from './components/Protected.jsx';
+import Signup from './pages/Signup.jsx';
+import Login from './pages/Login.jsx';
+import Home from './pages/Home.jsx'; // Replace with the correct path to Home component
+
+
+const router=createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element ={<App/>}>
+      <Route path="signup" element ={<Signup/>}/>
+      <Route path="login" element ={<Login/>}/>
+      <Route path="/" element ={<Protected/>}>
+        <Route path="/" index element={<Home/>}/>
+      </Route>
+    </Route>
+  )
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// Replace this line
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// With this line
+ReactDOM.render(<RouterProvider router={router} />, document.getElementById('root'));
+
